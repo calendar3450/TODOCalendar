8f6a265b0b48e48cd925710b757fbe43e29d1a9e
calendar3450 <mjk3450@naver.com>
2022-04-11 23:16:23 +0900

    처음...
    처음으로 만든 내 홈페이지 ㅠㅜㅜ

---
+++ momentum/js/greeting.js
@@ -0,0 +1,32 @@
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY="username";
+
+function onLoginSubmit(tomato){
+    tomato.preventDefault();// 새로고침을 안함 addEventListener에서 submit을 통해 새로고침을 해야하지만 못하게 막음
+    const username=loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // greeting.innerText="Hello "+username;
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreetings();
+}
+
+function paintGreetings(username){
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText= `Hello ${username}`;
+}
+
+loginForm.addEventListener("submit", onLoginSubmit)
+
+const savedUsername=localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}
+else{
+    paintGreetings(savedUsername);
+}
