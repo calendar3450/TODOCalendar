868de21ecf00902348a4253e1ce4182eb606c449
calendar3450 <75975464+calendar3450@users.noreply.github.com>
2022-04-13 16:38:35 +0900

    7.1 , 7.2장 수정
    7.1 7.2장 수정

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,10 +2,31 @@ const toDoForm =document.querySelector("#todo-form");
 const toDOInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteToDo(event){
+    const li =event.target.parentElement;
+    li.remove()
+}
+
+
+function paintToDo(newTodo){//todo리스트에 추가 시키기
+    const listTodo = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "✂︎";
+    button.addEventListener("click", deleteToDo);
+    listTodo.appendChild(span);
+    listTodo.appendChild(button);
+    
+    toDoList.appendChild(listTodo);
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDOInput.value;
     toDOInput.value="";
+    paintToDo(newTodo);
 }
+
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
