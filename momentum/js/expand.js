47af547b85e6d8b7496cc297e5bd83955b9c021e
calendar3450 <mjk3450@naver.com>
2022-05-02 00:55:19 +0900

    sideBar 뼈대 구축

--- momentum/js/expand.js
+++ momentum/js/expand.js
@@ -1,1 +1,21 @@
 const sideButton = document.querySelector('#expandButtonJS');
+const sideBar = document.querySelector('#sideBar');
+
+
+function init(){
+    sideButton.addEventListener('click',openSideBar);
+}
+
+function openSideBar(){
+    sideBar.style.transform= 'translateX(0)';
+    sideButton.removeEventListener('click',openSideBar);
+    sideButton.addEventListener('click',closeSideBar);
+}
+
+function closeSideBar() {
+    sideBar.style.transform= 'translateX(-100%)';
+    sideButton.removeEventListener('click',closeSideBar);
+    sideButton.addEventListener('click',openSideBar);
+}
+
+init()
